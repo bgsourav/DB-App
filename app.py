8c2e5c541e53493a37b9603a9b7ab4a6c297876f
BGSourav <bgsouravbg@gmail.com>
2022-11-08 23:44:30 +0530

    trailchanges

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,url_for,session,request,redirect
 import mysql.connector
 
 app = Flask(__name__)
-
+app.secret_key = "super secret key"
 connection = mysql.connector.connect(user="me",password="1234",host='127.0.0.1',port=3306)
 cursor = connection.cursor()
 cursor.execute("use EUManagementsystem;")
@@ -31,8 +31,6 @@ def que_exec():
                     "Please Try again !!!!!"
             head = []
         que = values
-        # que = str(que)
-        # x = que.split("), (")
         return render_template("input.html", data = que, Qp=tempo,table_headers = head)
     else:
         return render_template("input.html", data = [],Qp = prev_query,table_headers = [])
@@ -40,13 +38,21 @@ def que_exec():
 
 @app.route('/tab1',methods = ['GET','POST'])
 def show_countries():
+    session["table"] = "Countries"
     return render_template("countries.html")
 
 
 @app.route('/tab2',methods = ['GET','POST'])
 def show_deps():
+    session["table"] = "Departments"
     return render_template("departments.html")
 
 
+@app.route('/ins',methods = ['GET','POST'])
+def show_ins():
+    return render_template("insertform.html",count = session["table"])
+
+
 if __name__ == '__main__':
+    app.debug = True
     app.run()
